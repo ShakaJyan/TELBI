357524b0ee6bbf0f6abe2355b91038d710101636
ShakaJyan <90365260+ShakaJyan@users.noreply.github.com>
2021-09-17 21:06:29 +0900

    Update TELBI.py

--- TELBI.py
+++ TELBI.py
@@ -12,13 +12,10 @@ import telebot # pip install pyTelegramBotAPI
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import urllib.request
-import os
 import time
 from PIL import Image
 import schedule   #pip install schedule
 import threading
-import random
 from gtts import gTTS #pip install gTTS
 import pyautogui as pag  #pip install pyautogui
 import pywinauto    #pip install pywinauto
