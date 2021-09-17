7157149dc6f04c740fbc3ef85279ef2d0d3bd121
ShakaJyan <90365260+ShakaJyan@users.noreply.github.com>
2021-09-17 20:53:36 +0900

    Delete package directory

--- package/tts_package.py
+++
@@ -1,6 +0,0 @@
-from gtts import gTTS #pip install gTTS
-def tts(x,bot, chat_id):
-    tts = gTTS(x, lang='ko')
-    tts.save("x.ogg")
-    audio = open("x.ogg",'rb')
-    bot.send_audio(chat_id, audio=audio)
